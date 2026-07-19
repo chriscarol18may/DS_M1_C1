fb9065a6479c2915f702f6724314e3d34c87b239
chriscarol18may <78288189+chriscarol18may@users.noreply.github.com>
2026-07-19 12:38:22 +0530

    Create main_print_a1.py

---
+++ main_print_a1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Sarah.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love art & craft.")
+
+# Performing and printing a simple calculation
+print("The result of 5 + 5 is:", 5 + 5)
